e4c24bec407167e1557f245c6aa00b032f908fb4
hvnganh <102368559+hvnganh@users.noreply.github.com>
2022-09-24 13:32:42 +0700

    update bg

--- components/layout.js
+++ components/layout.js
@@ -10,14 +10,14 @@ export default function Layout({ children }) {
         <link rel="icon" href="/wedding-icon.png" />
       </Head>
       <div className="px-2 py-3 max-w-2xl">
-        <div className="bg-wedding_bg bg-contain bg-no-repeat h-[100vh] text-white">
-          {/* <div className="header">
+        <div className="bg-wedding_bg bg-contain bg-no-repeat h-[600px] text-white">
+          <div className="header">
             <Navbar />
-          </div> */}
-          {/* <main className="text-center content">{children}</main> */}
-          {/* <div className="footer">
+          </div>
+          <main className="text-center content">{children}</main>
+          <div className="footer">
             <Footer />
-          </div> */}
+          </div>
         </div>
 
       </div>
