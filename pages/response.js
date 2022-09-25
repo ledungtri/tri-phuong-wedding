c4373adf6e1bdd722fd670ffb4f374e70841aff7
hvnganh <102368559+hvnganh@users.noreply.github.com>
2022-09-25 16:12:40 +0700

    animate

--- pages/response.js
+++ pages/response.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function Response() {
-  const router = useRouter()
+  const router = useRouter();
   const [name, setName] = useState('');
   const [phone, setPhone] = useState(0);
   const [attending, setAttending] = useState();
@@ -25,8 +25,8 @@ export default function Response() {
     }
     try {
       const res = await axios.post("http://bglmautam.com:3030/responses", newUser)
-      console.log(res)
-      router.push('/thanks')
+        console.log(res);
+        router.push('/thanks');
     } catch (error) {
       console.log(error)
     }
@@ -35,33 +35,33 @@ export default function Response() {
     <div className='h-screen'>
       <div className="flex flex-col items-start px-4 h-full justify-evenly text-amber-200 m-auto">
         <div className="flex flex-col items-start w-full">
-          <h1 className="text-2xl font-bold">Thứ Hai, Ngày 1 Tháng 12</h1>
-          <h1 className="mt-2 text-2xl font-bold">Vào Lúc 19:00 PM</h1>
-          <h1 className="mt-2 text-2xl font-bold">Nhà Hàng Crystal Palace</h1>
+          <h1 className="date text-2xl font-bold">Thứ Hai, Ngày 1 Tháng 12</h1>
+          <h1 className="date mt-2 text-2xl font-bold">Vào Lúc 19:00 PM</h1>
+          <h1 className="date mt-2 text-2xl font-bold">Nhà Hàng Crystal Palace</h1>
         </div>
         <div className="w-full">
           <form className="flex flex-col" onSubmit={handleGoToMap}>
             <div className="flex flex-col mb-4 items-start">
-              <label className="font-semibold mb-1">Name *</label>
-              <input required onChange={e => setName(e.target.value)} className="rounded w-full py-1 px-2 text-black" placeholder="Your name..." />
+              <label className="font-semibold mb-1">Họ và Tên *</label>
+              <input required onChange={e => setName(e.target.value)} className="rounded w-full py-1 px-2 text-black" placeholder="Họ tên của bạn..." />
             </div>
             <div className="flex flex-col mb-4 items-start">
-              <label className="font-semibold mb-1">Phone *</label>
-              <input required onChange={e => setPhone(e.target.value)} className="rounded w-full py-1 px-2 text-black" placeholder="Phone number..." />
+              <label className="font-semibold mb-1">Số Điện Thoại *</label>
+              <input required onChange={e => setPhone(e.target.value)} className="rounded w-full py-1 px-2 text-black" placeholder="Số điện thoại..." />
             </div>
             <div className="flex flex-col mb-4 items-start">
-              <label className="font-semibold mb-1">Will you be attending? *</label>
+              <label className="font-semibold mb-1">Bạn sẽ tham dự chứ ? *</label>
               <select onChange={e => handleAttending(e)} className="w-[50%] text-black h-8 rounded px-1" id="attending" name="attending">
-                <option value={true}>Yes</option>
-                <option value={false}>No</option>
+                <option value={true}>Tham gia</option>
+                <option value={false}>Hông tham gia</option>
               </select>
             </div>
             <div className="flex flex-col mb-8 items-start">
-              <label className="font-semibold mb-1">Bringing a +1 ? *</label>
-              <input onChange={e => setGuest(e.target.value)} className="rounded w-full py-1 px-2 text-black" placeholder="Name of Guest" />
+              <label className="font-semibold mb-1">Bạn có +1 thêm không ?</label>
+              <input onChange={e => setGuest(e.target.value)} className="rounded w-full py-1 px-2 text-black" placeholder="Cho mình biết số lượng nhé" />
             </div>
             <div className="w-full bg-amber-200 rounded h-10 flex items-center justify-center">
-              <button type="submit" className="uppercase text-white font-bold">Send your answer</button>
+              <button type="submit" className="uppercase text-white font-bold">Gửi câu trả lời</button>
             </div>
           </form>
         </div>
--- pages/thanks.js
+++ pages/thanks.js
@@ -5,13 +5,13 @@ function Thank() {
   return (
     <div className="flex h-screen justify-center bg-thanks_bg bg-cover bg-center">
       <div className="mt-[50%]">
-        <div className="mb-4">
+        <div className="mb-8">
           <h1 className="text-2xl uppercase italic font-bold">
             chân thành cám ơn bạn
           </h1>
         </div>
         <div>
-          <button className="border-solid border-amber-200 text-amber-200 font-bold text-xl capitalize border-[4px] w-[260px] h-[50px] rounded-3xl m-2">
+          <button className="border-solid border-amber-200 text-amber-200 font-bold text-xl capitalize border-[4px] w-[260px] h-[50px] rounded-3xl m-2 animate-bounce">
             <Link href="map">BẢN ĐỒ ĐƯỜNG ĐI</Link>
           </button>
         </div>
